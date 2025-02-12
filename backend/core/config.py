239cc4c7173d964c65c8992f152c8a7f943d7c08
Jon-Hurley <jonwhurley@gmail.com>
2025-02-12 17:32:10 -0500

    updated config and up to date with main

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,7 +1,7 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    PROJECT_NAME: str = "FastAPI MongoDB App"
+    PROJECT_NAME: str = "SafeEats Backend"
     MONGODB_URI: str
     MONGODB_NAME: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
