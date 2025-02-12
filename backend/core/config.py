7188be9e864fd77cb380f8f5396dda9bdb1cada7
Jon-Hurley <jonwhurley@gmail.com>
2025-02-12 01:05:13 -0500

    change to settings class

--- backend/core/config.py
+++ backend/core/config.py
@@ -4,7 +4,6 @@ class Settings(BaseSettings):
     PROJECT_NAME: str = "FastAPI MongoDB App"
     MONGODB_URI: str
     MONGO_DB_NAME: str
-    SECRET_KEY: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
 
     class Config:
