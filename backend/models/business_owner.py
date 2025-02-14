8bb8fe736864b3893cf4e4920a37e9eeee7895ee
aeschran <avaschrandt@gmail.com>
2025-02-13 23:26:31 -0500

    updating business owner model

--- backend/models/business_owner.py
+++ backend/models/business_owner.py
@@ -1,9 +1,9 @@
-# models/user.py
+# models/business_owner.py
 from motor.motor_asyncio import AsyncIOMotorClient
 from bson import ObjectId
 
 class BusinessOwner:
-    def __init__(self, name: str, email: str, password: str, isVerified: bool):
+    def __init__(self, name: str, email: str, password: str, isVerified: bool = False):
         self.name = name
         self.email = email
         self.password = password
