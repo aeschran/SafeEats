b4736ad57e02f0930097819d6cd75101b9356b67
Jon-Hurley <jonwhurley@gmail.com>
2025-02-19 10:22:58 -0500

    Create Map.swift

---
+++ frontend/SafeEats/Views/Map/Map.swift
@@ -0,0 +1,18 @@
+//
+//  Map.swift
+//  SafeEats
+//
+//  Created by Jon Hurley on 2/19/25.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
