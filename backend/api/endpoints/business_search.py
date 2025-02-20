ece5d762adbecc2a7d57ce7affd4c73498e7622e
Jon-Hurley <jonwhurley@gmail.com>
2025-02-19 19:53:27 -0500

    change to search business endpoint

--- backend/api/endpoints/business_search.py
+++ backend/api/endpoints/business_search.py
@@ -7,5 +7,5 @@ router = APIRouter(tags=["Business Search"])
 business_search_service = BusinessSearchService()
 
 @router.get("")
-async def search_businesses_endpont(query: str, near: str, limit: int = 10):
+async def search_businesses_endpont(query: str = "coffee", near: str = "West Lafayette, IN", limit: int = 10):
     return business_search_service.search_by_query()
