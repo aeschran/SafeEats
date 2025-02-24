5310da277193532a5c27e75436ecd0fdcbfde9b8
Jon-Hurley <jonwhurley@gmail.com>
2025-02-24 16:46:42 -0500

    added db checking for preferences

--- backend/services/preference_service.py
+++ backend/services/preference_service.py
@@ -10,6 +10,9 @@ class PreferenceService(BaseService):
         
     async def create_new_preference(self, preference_create: PreferenceCreate):
         preference = Preference(preference=preference_create.preference, preference_type=preference_create.preference_type)
+        result = await self.db.preferences.find_one({"preference": preference_create.preference, "preference_type": preference_create.preference_type})
+        if result:
+            return None
         result = await self.db.preferences.insert_one(preference.to_dict())
         if result.inserted_id:
             return PreferenceResponse(**preference.to_dict())
