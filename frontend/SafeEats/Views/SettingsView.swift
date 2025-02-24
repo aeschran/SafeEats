4219fccb1e1c4c63e6029fb12180d2ba3cbf8b9d
Jack Rookstool <89651782+jrookstool@users.noreply.github.com>
2025-02-24 18:31:08 -0500

    added ui for suggesting preferences, next is cross-reference with list of preferences from db

--- frontend/SafeEats/Views/SettingsView.swift
+++ frontend/SafeEats/Views/SettingsView.swift
@@ -12,6 +12,7 @@ struct SettingsView: View {
     @State private var showChangePassword = false
     @State private var showDeleteAccountAlert = false
     @State private var showLogoutConfirmation = false
+    @State private var tags : [Tag] = []
     
     @EnvironmentObject var authViewModel: AuthViewModel
     
@@ -62,11 +63,16 @@ struct SettingsView: View {
                         }
                     }
                     
+                    GroupBox(label: Label("Suggest New Preferences", systemImage: "fork.knife.circle.fill")) {
+                            TagField(tags: $tags)
+                        }
+                    
                     
                 }
                 .scrollContentBackground(.hidden)
                 .background(Color.mainGray)
-                
+
+                }
                 Text("Have any questions? Email us at safeeats.dev@gmail.com!")
             }
             .background(Color(UIColor.systemGray6))
@@ -85,6 +91,93 @@ struct SettingsView: View {
     }
     
     
+
+
+struct TagField: View {
+    @Binding var tags: [Tag]
+    var body : some View {
+        VStack {
+            VStack {
+                        ForEach($tags) { tag in TagView(tag: tag, allTags: $tags)
+                        }
+                    }
+                    .padding(.vertical, 10)
+                    .padding(.horizontal, 15)
+                    .background(.bar, in: .rect(cornerRadius: 12))
+                    .onAppear {
+                        if tags.isEmpty {
+                            tags.append(.init(value: "", isInitial: true))
+                        }
+                    }
+            Button { Task {
+                    print("Tags: \(tags)")
+                
+                    // TODO: get preference list from backend, compare all tags with those, report good or bad! If good, send email, if bad, report error.
+                }
+            } label: {
+                Text("Submit Suggestions")
+                    .fontWeight(.semibold)
+                    .foregroundStyle(.white)
+                    .padding(.vertical, 12)
+                    .padding(.horizontal, 20)
+                    .background(
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 15)
+                                .fill(Color.mainGreen)
+                                .zIndex(1)
+                        }
+                    )
+            }
+        }
+        
+    }
+        
+}
+
+fileprivate struct TagView: View {
+    @Binding var tag: Tag
+    @Binding var allTags: [Tag]
+    @FocusState private var isFocused: Bool
+    @Environment(\.colorScheme) private var colorScheme
+    var body: some View {
+        TextField("New Preference", text: $tag.value)
+            .focused($isFocused)
+            .padding(.vertical, 10)
+            .padding(.horizontal, 10)
+            .background((colorScheme == .dark ? Color.black : Color.white).opacity(isFocused ? 0 : 1), in: .rect(cornerRadius: 5))
+            .disabled(tag.isInitial)
+            .overlay {
+                if tag.isInitial {
+                    Rectangle()
+                        .fill(.clear)
+                        .contentShape(.rect)
+                        .onTapGesture {
+                            tag.isInitial = false
+                            isFocused = true
+                        }
+                }
+            }
+            .onSubmit {
+                addNewTag()
+            }
+                
+    }
+    
+    private func addNewTag() {
+        guard !tag.value.trimmingCharacters(in: .whitespaces).isEmpty else { return }
+        
+        print("Tag: \(tag.value), Adding new tag to Tags: \(allTags)")
+        
+        if allTags.last?.value != "" {
+                        allTags.append(Tag(value: "", isInitial: true))
+                    }
+            
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+            if let lastIndex = allTags.indices.last {
+                isFocused = (allTags[lastIndex].isInitial)
+            }
+        }
+    }
 }
 
 #Preview {
