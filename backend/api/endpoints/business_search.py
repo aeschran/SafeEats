d90b787da3a2b4db7beb7096f90079db4b2ee4c1
Jon-Hurley <jonwhurley@gmail.com>
2025-02-24 16:58:25 -0500

    spelling error

--- backend/api/endpoints/business_search.py
+++ backend/api/endpoints/business_search.py
@@ -6,5 +6,5 @@ router = APIRouter(tags=["Business Search"])
 business_search_service = BusinessSearchService()
 
 @router.get("")
-async def search_businesses_endpont(query: str = "coffee", near: str = "West Lafayette, IN", limit: int = 10):
+async def search_businesses_endpoint(query: str = "coffee", near: str = "West Lafayette, IN", limit: int = 10):
     return business_search_service.search_by_query()
